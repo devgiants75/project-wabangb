6929b78a37d9f6ed2bf380d89890fa43ed6533c3
Anchansook <154656462+Anchansook@users.noreply.github.com>
2024-06-29 15:21:05 +0900

    20240629 produce files acs

---
+++ src/views/ReservationPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReservationPage() {
+  return (
+    <div>ReservationPage</div>
+  )
+}
---
+++ src/views/UsageDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UsageDetails() {
+  return (
+    <div>UsageDetails</div>
+  )
+}
