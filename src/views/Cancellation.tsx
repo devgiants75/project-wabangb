05963bde3191c6982813cb1fe9cb8d0d71145ed8
Anchansook <154656462+Anchansook@users.noreply.github.com>
2024-07-14 12:28:47 +0900

    20240714 reservationList & usageDetails & cancellation acs

--- src/views/Cancellation.tsx
+++ src/views/Cancellation.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import MenuBarReservation from '../components/MenuBarReservation'
+import '../styles/reset.css';
+import '../styles/cancellation.css';
 
 export default function Cancellation() {
   return (
-    <div>Cancellation</div>
+    <>
+      <Header />
+      <MenuBarReservation />
+
+      <main id='cancelWrap'>
+        <div className='refundAmountBox'>
+          <div className='amountTop'>예상 환불 금액</div>
+          <div className='amountBottom'>
+            <span>1,000,000</span>
+            <span>원</span>
+          </div>
+        </div>
+        <div className='RefundPolicyBox'></div>
+        <div className='requestAndAgreeBox'></div>
+      </main>
+
+      <Footer />
+    </>
   )
 }
