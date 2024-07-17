b6fb01609e3bbe2b0791f04f7b7d0970ad3e6094
CodeDDAGGI <qudrb1132@naver.com>
2024-07-17 14:14:22 +0900

    0717

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -3,12 +3,15 @@ import Header from '../components/Header'
 import Main_Search from '../components/MainSearch'
 import ContrySelect from '../components/ContrySelect'
 import Footer from '../components/Footer'
+import  '../styles/기장.jpg'
+import  '../styles/울산.jpg'
+import  '../styles/부산.jpg'
 
 export default function HomePage() {
   const images = [
-    '/asset/gijang.jpg',
-    '/asset/ulsan.jpg',
-    '/asset/busan.jpg'
+    '/asset/기장.jpg',
+    '/asset/울산.jpg',
+    '/asset/부산.jpg'
   ];
   return (
     <div>
